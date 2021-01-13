608f32de3f11cc2459d91b53639f32e4736fd4e1
Tarepan <tarepan5884@gmail.com>
2021-01-13 08:42:27 +0000

    Fix with origin v0.2

--- rnnms/dataset.py
+++ rnnms/dataset.py
@@ -1,49 +1,51 @@
+from pathlib import Path
+import random
+import json
+
 import numpy as np
 import torch
-import os
-from random import randint
 from torch.utils.data import Dataset
 
 
 class VocoderDataset(Dataset):
-    def __init__(self, meta_file: str, sample_frames: int, audio_slice_frames: int, hop_length: int):
+    def __init__(self, root, sample_frames=24, hop_length=200):
         """
         Args:
-            meta_file: data path info
             sample_frames: Clipped Mel-spectrogram length
-            audio_slice_frames: Clipped audio length in the unit of Mel-spectrogram (`3` == `3*hop_length` audio seq)
             hop_length: STFT stride
         """
 
+        self.root = Path(root)
         self.sample_frames = sample_frames
-        self.audio_slice_frames = audio_slice_frames
-        self.pad = (sample_frames - audio_slice_frames) // 2
         self.hop_length = hop_length
 
         # Extract data pathes from meta file
-         with open(meta_file, encoding="utf-8") as f:
-            self.metadata = [line.strip().split("|") for line in f]
-        self.metadata = [m for m in self.metadata if int(m[3]) > 2 * sample_frames - audio_slice_frames]
+        metadata_path = self.root / "train.json"
+        with open(metadata_path) as file:
+            metadata = json.load(file)
+            self.metadata = [Path(path) for _, path in metadata]
 
     def __len__(self):
         return len(self.metadata)
 
-    def __getitem__(self, index):
-        _, audio_path, mel_path, _ = self.metadata[index]
+    def __getitem__(self, index: int):
+        path = self.root / self.metadata[index]
 
-        audio = np.load(os.path.join(audio_path))
-        mel = np.load(os.path.join(mel_path))
+        audio = np.load(path.with_suffix(".wav.npy"))
+        mel = np.load(path.with_suffix(".mel.npy"))
 
-        # fixed length clipping
-        # pos: start
-        pos = randint(0, len(mel) - self.sample_frames)
+        start = random.randint(0, mel.shape[-1] - self.sample_frames - 1)
 
+        # Mel-spectrogram clipping
+        start_mel = start
+        end_mel = start + self.sample_frames
         # (T, freq) -> (sample_frames, freq)
-        mel = mel[pos:pos + self.sample_frames, :]
+        mel = mel[:, start_mel : start + end_mel]
 
-        # (S,) -> (hop_length * audio_slice_frames,)
-        p_audio = self.hop_length * (pos + self.pad)
-        q_audio = self.hop_length * (pos + self.pad + self.audio_slice_frames) + 1
-        audio = audio[p_audio:q_audio]
+        # Waveform clipping
+        start_wave = self.hop_length * start
+        end_wave = self.hop_length * (start + self.sample_frames) + 1
+        # (S,) -> (samle_frames * hop_length,)
+        audio = audio[start_wave : end_wave]
 
-        return torch.LongTensor(audio), torch.FloatTensor(mel)
+        return torch.LongTensor(audio), torch.FloatTensor(mel.T)
